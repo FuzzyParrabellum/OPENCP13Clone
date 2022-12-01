388e52811b80e0b7b69d9740225c0b394935ae7e
notRoot <kalimba@pc-34.home>
2022-12-01 13:58:47 +0100

    moved old urls to news apps lettings and profiles, also changed templates names and used namespacing

--- lettings/views.py
+++ lettings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Letting
 
 
@@ -6,10 +7,10 @@ from .models import Letting
 # Sed non placerat massa. Integer est nunc, pulvinar a
 # tempor et, bibendum id arcu. Vestibulum ante ipsum primis in faucibus orci
 # luctus et ultrices posuere cubilia curae; Cras eget scelerisque
-def lettings_index(request):
+def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
+    return render(request, 'lettings/index.html', context)
 
 
 # Cras ultricies dignissim purus, vitae hendrerit ex varius non. In accumsan
@@ -30,4 +31,4 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)
