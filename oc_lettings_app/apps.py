c9283c32d1a491deb80f95144fe1b30c49abf9fa
notRoot <kalimba@pc-34.home>
2022-12-02 08:21:42 +0100

    Added an oc_lettings_app, transferred tests, views, etc. from the proejct oc_lettings_site, also refactored url schema

---
+++ oc_lettings_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OcLettingsAppConfig(AppConfig):
+    name = 'oc_lettings_app'
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
-
-
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('oc_lettings_app.urls')),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls'))
 ]
