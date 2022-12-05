64060307f6f01eb4e2e79a4f8bfb3664547a7b1c
notRoot <kalimba@pc-34.home>
2022-12-05 14:12:32 +0100

    Added sdk config, downloaded sdk-sentry dependancy, put secret_key and dsn key in .env, removed docker-compose.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,15 +1,35 @@
 import os
+
+from django.core.management.utils import get_random_secret_key
+
 import django_on_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+sentry_sdk.init(
+    dsn=os.environ.get('dsn'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('oc_lettings_app.urls')),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls'))
+]
+
+
+# Sentry test debug route
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
+
+
+urlpatterns += [
+    path('sentry-debug/', trigger_error),
 ]
