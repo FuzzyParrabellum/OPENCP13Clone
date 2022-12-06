d60b0cb31ca6be4250a8c21250dd1df3eb21e1a9
notRoot <kalimba@pc-34.home>
2022-12-06 14:16:07 +0100

    Refactored code, removed Procfile

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -6,9 +6,11 @@ import django_on_heroku
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Sentry integration to monitor error alerts
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_DSN'),
     integrations=[
